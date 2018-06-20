28ec74fbd71f9905af08889cb565bd2f8b968af0
curlyz <curly.saigonese@gmail.com>
2018-06-21 03:22:45 +0700

    Update Weather.py

--- branch/TheShield/3/Blocky/Weather.py
+++ branch/TheShield/3/Blocky/Weather.py
@@ -1,1 +1,23 @@
-s
+from machine import Pin
+from dht import *
+from Blocky.Pin import getPin
+
+class Weather:
+	def __init__ (self , port,type='DHT11'):
+		pin = getPin(port)
+		if (pin[0] == None):
+			from machine import reset
+			reset()
+		if type == 'DHT11': self.weather = DHT11(Pin(pin[0]))
+		elif type == 'DHT22': self.weather = DHT22(Pin(pin[0]))
+		elif type == 'DHTBase': self.weather = DHTBase(Pin(pin[0]))
+		else :
+			raise NameError
+	  
+	def temperature (self):
+		self.weather.measure()
+		return self.weather.temperature()
+	def humidity(self):
+		self.weather.measure()
+		return self.weather.humidity()
+   
