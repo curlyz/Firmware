449f5dbdbe8fd7c9220ff98f3970d806bf625fac
curlyz <curly.saigonese@gmail.com>
2018-06-20 23:03:29 +0700

    Create Smoke.py

---
+++ branch/TheShield/3/Blocky/Smoke.py
@@ -0,0 +1,17 @@
+from machine import ADC , Pin 
+from Blocky.Pin import getPin
+class Smoke :
+	def __init__ (self , port , sensitivity = 4):
+		pin = getPin(port)
+		if (pin[2] == None):
+			from machine import reset
+			reset()
+		
+		self.adc = ADC(Pin(pin[2]))
+		if (sensitivity == 1): self.adc.atten(ADC.ATTN_0DB)
+		elif (sensitivity == 2): self.adc.atten(ADC.ATTN_2_5DB)
+		elif (sensitivity == 3): self.adc.atten(ADC.ATTN_6DB)
+		elif (sensitivity == 4): self.adc.atten(ADC.ATTN_11DB)
+	def read(self):
+		return self.adc.read()
+		
