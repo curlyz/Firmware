f84b7dd7adb7df630429e68a08889beeaf412a62
curlyz <curly.saigonese@gmail.com>
2018-06-21 03:24:28 +0700

    Update IFTTT.py

--- branch/TheShield/3/Blocky/IFTTT.py
+++ branch/TheShield/3/Blocky/IFTTT.py
@@ -1,1 +1,12 @@
-s
+import urequests
+
+class IFTTT:
+	def __init__ (self,key):
+		self.key = key 
+	def send(self,topic,data=None):
+		try :
+			urequests.get('https://maker.ifttt.com/trigger/' + topic + '/with/key/'+self.key)
+		except:
+			pass
+			
+
